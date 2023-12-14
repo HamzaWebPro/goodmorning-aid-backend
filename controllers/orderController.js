944ec0ef3e2213048421b4fc26cd584ce6836e1a
HamzaWebPro <hamza.mern2201@gmail.com>
2023-12-14 16:30:01 +0600

    gateway added

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -65,12 +65,8 @@ const createOrder = async (req, res) => {
             onlinePay,
             outsideCity,
           },
-          redirect_url: encodeURIComponent(
-            "https://landing-page-beta-topaz.vercel.app"
-          ),
-          cancel_url: encodeURIComponent(
-            "https://landing-page-beta-topaz.vercel.app"
-          ),
+          redirect_url: "https://landing-page-beta-topaz.vercel.app/success",
+          cancel_url: "https://landing-page-beta-topaz.vercel.app",
         },
       };
 
